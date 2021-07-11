4e63f7b349d6a499a2d7fd5bdfeca754169f3111
sebastianchoo <sebastian_choo>
2021-07-12 02:10:42 +0800

    Added Flask API URI

--- constants/API.js
+++ constants/API.js
@@ -1,4 +1,4 @@
-export const API = "https://username.pythonanywhere.com"
+export const API = "https://saltyseb.pythonanywhere.com"
 export const API_LOGIN = "/auth"
 export const API_SIGNUP = "/newuser"
 export const API_POSTS = "/posts"
