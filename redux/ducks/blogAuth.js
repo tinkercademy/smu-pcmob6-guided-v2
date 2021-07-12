cd57e4f9cf09a1095e5ed04932bbac58068f0f3a
sebastianchoo <sebastian_choo>
2021-07-12 19:13:52 +0800

    Setting up redux store

---
+++ redux/ducks/blogAuth.js
@@ -0,0 +1,23 @@
+export const LOG_IN = "log_in";
+export const LOG_OUT = "log_out";
+
+export function logInAction() {
+  return { type: LOG_IN }
+}
+
+export function logOutAction() {
+  return { type: LOG_OUT }
+}
+
+const initialState = {
+  auth: null,
+}
+
+export default function blogAuthReducer(state = initialState) {
+  switch (action.type) {
+    case "log_in":
+      return { ...state, auth: action.payload }
+    case "log_out":
+      return { ...state, auth: null }
+  }
+}
