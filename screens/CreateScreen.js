6654e5befde9536b4b5fb8b60e2c3003c546817e
sebastianchoo <sebastian_choo>
2021-07-12 17:18:59 +0800

    CreateScreen Task 1
    Task 1a - Forgot to add savePost function

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -7,14 +7,36 @@ import { lightStyles, commonStyles } from "../styles/commonStyles";
 
 export default function CreateScreen({ navigation }) {
 
-  const styles = {...lightStyles, ...commonStyles}
+  const styles = { ...lightStyles, ...commonStyles }
+  
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
+  function savePost() {
+    
+  }
 
   return (
     <View style={styles.container}>
-      <Text>
-        Create Post Screen
-      </Text>
+      <View style={{ margin: 20 }}>
+        <Text style={[additionalStyles.label, styles.text]}>Enter Title:</Text>
+        <TextInput
+          style={additionalStyles.input}
+          value={title}
+          onChangeText={text => setTitle(text)}
+        />
+        <Text style={[additionalStyles.label, styles.text]}>Enter Content:</Text>
+        <TextInput
+          style={additionalStyles.input}
+          value={content}
+          onChangeText={text => setContent(text)}
+        />
+      <TouchableOpacity style={[styles.button, {marginTop: 20}]} onPress={savePost}>
+        <Text style={styles.buttonText}>
+          Save
+        </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
